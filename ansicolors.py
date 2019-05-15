4f87b758584661b576532f21232d31f72d186765
Brady O'Leary <thebradyoleary@gmail.com>
2019-05-15 14:54:27 -0400

    Added Various Text Features and made colors work correctly

--- ansicolors.py
+++ ansicolors.py
@@ -16,7 +16,13 @@ class Text:
     reset = False #0
     bold = False #1
     faint = False #2
-    underline = False #3
+    italic = False #3 (Not Widely Supported)
+    underline = False #4
+    slowBlink = False #5
+    rapidBlink = False #6 (not widely supported)
+    reverseVideo = False #7
+    conceal = False #8 (Not Widely Supported)
+    crossedOut = False #9
     color_fg = 0 #30-37, 38, 39, 90-97
     color_fg_rgb = False # For option 38 (array) and 39 (integer), or False if unused
     color_bg = 0 #40-47, 48, 49, 100-107
@@ -41,16 +47,45 @@ class Text:
     BRIGHT_CYAN = (96, 106)
     BRIGHT_WHITE = (97, 107)
 
-    def __init__(self, reset=False, bold=False, faint=False, underline=False):
+    def __init__(self, reset=False, bold=False, faint=False, italic=False, underline=False, slowBlink=False, rapidBlink=False, reverseVideo=False, conceal=False, crossedOut=False):
         if reset:
             reset = True
         if bold:
             self.bold = True
         if faint:
             self.faint = True
+        if italic:
+            self.italic = True
         if underline:
             self.underline = True
+        if slowBlink:
+            self.slowBlink = True
+        if rapidBlink:
+            self.rapidBlink = True
+        if reverseVideo:
+            self.reverseVideo = True
+        if conceal:
+            self.conceal = True
+        if crossedOut:
+            self.crossedOut = True
     
+    # Resets all Paramters in Object
+    def resetAll(self):
+        self.reset = False #0
+        self.bold = False #1
+        self.faint = False #2
+        self.italic = False #3 (Not Widely Supported)
+        self.underline = False #4
+        self.slowBlink = False #5
+        self.rapidBlink = False #6 (not widely supported)
+        self.reverseVideo = False #7
+        self.conceal = False #8 (Not Widely Supported)
+        self.crossedOut = False #9
+        self.color_bg = 0
+        self.color_fg = 0
+        self.color_bg_rgb = False
+        self.color_fg_rgb = False
+
     # Set Value of Bold
     # @param val (boolean) - If True, then text is bold
     def setBold(self, val):
@@ -76,12 +111,60 @@ class Text:
             self.underline = False
 
     # Reset all Effects
-    # @param val (boolean) - If True, then text Effects are Reset
+    # @param val (boolean) - If True, then add Reset Effect
     def setReset(self, val):
         if val:
             self.reset = True
         elif not val:
             self.reset = False
+
+    # Italicize all text
+    # @param val (boolean) - If True, then text is italicized
+    def setItalic(self, val):
+        if val:
+            self.italic = True
+        elif not val:
+            self.italic = False
+
+    # Make text Blink Slowly
+    # @param val (boolean) - If True, then text will blink
+    def setSlowBlink(self, val):
+        if val:
+            self.slowBlink = True
+        elif not val:
+            self.slowBlink = False
+
+    # Make Text Blink Rapidly
+    # @param val (boolean) - If True, then text will blink rapidly
+    def setRapidBlink(self, val):
+        if val:
+            self.rapidBlink = True
+        elif not val:
+            self.rapidBlink = False
+
+    # Reverses Background and Foreground Colors
+    # @param val (boolean) - If True, then text Colors are Reversed
+    def setReverseVideo(self, val):
+        if val:
+            self.reverseVideo = True
+        elif not val:
+            self.reverseVideo = False
+
+    # Conceal the Text
+    # @param val (boolean) - If True, then text is Concealed
+    def setConceal(self, val):
+        if val:
+            self.conceal = True
+        elif not val:
+            self.conceal = False
+
+    # Cross Out Text
+    # @param val (boolean) - If True, then text Effects are Reset
+    def setCrossedOut(self, val):
+        if val:
+            self.crossedOut = True
+        elif not val:
+            self.crossedOut = False
 
     # Set the Foreground Color of the Text
     # @param color (int or (int, int) from pre-defined color values) - Color to change the text to
@@ -90,7 +173,7 @@ class Text:
         val = -1
         if color == -1:
             self.color_fg_rgb = False
-            self.color_fg = -1
+            self.color_fg = 0
             return
         if isinstance(color, tuple):
             val = color[0]
@@ -128,7 +211,7 @@ class Text:
         # Check for Color Definition
         if color == -1:
             self.color_bg_rgb = False
-            self.color_bg = -1
+            self.color_bg = 0
             return
         val = -1
         if isinstance(color, tuple):
@@ -175,26 +258,50 @@ class Text:
             if len(s) > 0:
                 s += ";"
             s += "2"
-        if self.underline:
+        if self.italic:
             if len(s) > 0:
                 s += ";"
             s += "3"
+        if self.underline:
+            if len(s) > 0:
+                s += ";"
+            s += "4"
+        if self.slowBlink:
+            if len(s) > 0:
+                s += ";"
+            s += "5"
+        if self.rapidBlink:
+            if len(s) > 0:
+                s += ";"
+            s += "6"
+        if self.reverseVideo:
+            if len(s) > 0:
+                s += ";"
+            s += "7"
+        if self.conceal:
+            if len(s) > 0:
+                s += ";"
+            s += "8"
+        if self.crossedOut:
+            if len(s) > 0:
+                s += ";"
+            s += "9"
         if self.color_fg > 0:
             if len(s) > 0:
                 s += ";"
             if self.color_fg == 38 and self.color_fg_rgb != False and type(self.color_fg_rgb) is list:
                 s += "38;2;" + str(self.color_fg_rgb[0]) + ";" + str(self.color_fg_rgb[1]) + ";" + str(self.color_fg_rgb[2])
             elif self.color_fg == 38 and self.color_fg_rgb != False and type(self.color_fg_rgb) is int:
-                s += "33;5" + str(self.color_fg_rgb)
+                s += "38;5;" + str(self.color_fg_rgb)
             else:
                 s += str(self.color_fg)
         if self.color_bg > 0:
             if len(s) > 0:
                 s += ";"
-            if self.color_bg == 38 and self.color_bg_rgb != False and type(self.color_bg_rgb) is list:
-                s += "38;2;" + str(self.color_bg_rgb[0]) + ";" + str(self.color_bg_rgb[1]) + ";" + str(self.color_bg_rgb[2])
-            elif self.color_bg == 38 and self.color_bg_rgb != False and type(self.color_bg_rgb) is int:
-                s += "33;5" + str(self.color_bg_rgb)
+            if self.color_bg == 48 and self.color_bg_rgb != False and type(self.color_bg_rgb) is list:
+                s += "48;2;" + str(self.color_bg_rgb[0]) + ";" + str(self.color_bg_rgb[1]) + ";" + str(self.color_bg_rgb[2])
+            elif self.color_bg == 48 and self.color_bg_rgb != False and type(self.color_bg_rgb) is int:
+                s += "48;5;" + str(self.color_bg_rgb)
             else:
                 s += str(self.color_bg)
         s = ESCAPE + s + END
@@ -218,6 +325,62 @@ def test():
     ansi.setFGColorRGB(r=20, g=50, b=130)
     print(ansi.getColoredText("Hello World"))
     print("I should be normal ;)")
+
+    ansi = Text()
+    print(ansi.getColoredText("Normal Text"))
+    ansi.setBold(True)
+    print(ansi.getColoredText("Bold"))
+    ansi.setBold(False)
+    ansi.setFaint(True)
+    print(ansi.getColoredText("Faint"))
+    ansi.setFaint(False)
+    ansi.setItalic(True)
+    print(ansi.getColoredText("Italic"))
+    ansi.setItalic(False)
+    ansi.setUnderline(True)
+    print(ansi.getColoredText("Underline"))
+    ansi.setUnderline(False)
+    ansi.setSlowBlink(True)
+    print(ansi.getColoredText("Slow Blink"))
+    ansi.setSlowBlink(False)
+    ansi.setRapidBlink(True)
+    print(ansi.getColoredText("Rapid Blink"))
+    ansi.setRapidBlink(False)
+    ansi.setReverseVideo(True)
+    print(ansi.getColoredText("Reverse Video"))
+    ansi.setReverseVideo(False)
+    ansi.setConceal(True)
+    print(ansi.getColoredText("Concealed"))
+    ansi.setConceal(False)
+    ansi.setCrossedOut(True)
+    print(ansi.getColoredText("Crossed-Out"))
+    ansi.setCrossedOut(False)
+
+    ansi.setBold(True)
+    ansi.setFGColor(ansi.BRIGHT_MAGENTA)
+    ansi.resetAll()
+    print(ansi.getColoredText("This should be normal Text"))
+    
+    print("Testing N Colors...")
+    s = ""
+    for i in range(0,255):
+        ansi.setFGColorN(i)
+        ansi.setBGColorN(255 - i)
+        s += ansi.getColoredText(str(i)) + " "
+    print(s)
+    ansi.resetAll()
+
+    print("Testing RGB Colors...")
+    s = ""
+    for i in range(0,255):
+        for j in range(0,255):
+            for k in range(0,255):
+                ansi.setFGColorRGB(r=i, g=j, b=k)
+                ansi.setFGColorRGB(r=255-i, g=255-j, b=255-k)
+                s += ansi.getColoredText("[" + str(i) + "," + str(j) + "," + str(k) + "]") + " "
+    print(s)
+
+    print("Testing Finished.")
 
 if __name__ == "__main__":
     test()
